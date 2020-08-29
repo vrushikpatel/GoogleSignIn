9c8f555e68b6ad269f4ba3de618dae094c203372
vrushikpatel <vrushik0312@gmail.com>
2020-08-29 09:17:26 +0530

    practicing google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -11,30 +15,90 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _init=false;
-  void intializeFirebase()async{
-    await Firebase.initializeApp();
-    _init=true;
-  }
-  @override
-  void initState() {
-    intializeFirebase();
-    super.initState();
-  }
+
+ FirebaseAuth  auth=FirebaseAuth.instance;
+ GoogleSignIn googleSignIn=GoogleSignIn();
+ FirebaseUser users;
+ bool isSign = false;
+  
+// sign in
+Future<void> signinhandle()async{
+ GoogleSignInAccount googlesigninaccount= await googleSignIn.signIn(); 
+ GoogleSignInAuthentication googleSignInAuthentication=await googlesigninaccount.authentication;
+
+ final AuthCredential authCredential=GoogleAuthProvider.getCredential(
+   idToken: googleSignInAuthentication.idToken , accessToken: googleSignInAuthentication.accessToken);
+
+
+   final UserCredential authResult = await auth.signInWithCredential(authCredential) ;
+ 
+
+   users = authResult.user;
+
+   setState(() {
+     isSign=true;
+   });
+   
+}
+
+//sign out
+Future<void>signout()async{
+
+await auth.signOut().then((value) {
+googleSignIn.signOut();
+setState(() {
+  isSign=false;
+});
+});
+}
+
+  
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: (){
-
-          if(_init){
-            print('button pressed and firebase initialized');
-          }
-          else
-            print('button pressed');
-        },
-        splashColor: Colors.blueAccent,
-        child: Text('Sign In with google'),
+    return MaterialApp(
+      color: Colors.blueAccent,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('Google Sign In'),),
+          ),
+          body:Center(
+             child:  isSign ? Column(
+                children: [
+                  CircleAvatar(
+                    backgroundImage:NetworkImage(users.photoUrl) ,
+                    radius: 50.0,
+                  ),
+                  Text(users.displayName),
+                  Text(users.email),
+                  FlatButton(
+                    onPressed: (){
+                      signout();
+                    },
+                    child: Text('Sign Out'),
+                     ),
+               ],
+             ) 
+             : 
+             Container(
+              width: 100.0,
+              height: 100.0,
+              decoration: BoxDecoration(
+                color: Colors.amberAccent,
+                borderRadius: BorderRadius.circular(20.0)
+              ),
+              child: Center(
+                  child: FlatButton(
+                      onPressed: (){ 
+                        print('sign in pressed');
+                          signinhandle();
+                      },
+                      child: Text('Click Here to Sign In'),
+                  ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
